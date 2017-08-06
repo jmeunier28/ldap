f1339254cf48facb9356a4efa61d14f0d1edf419
= <jmeunier@bu.edu>
2017-08-06 13:58:25 -0700

    updating for installing openldap

--- provision.sh
+++ provision.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-sudo yum -y install httpd
-
+sudo su
+yum -y install httpd
+yum -y install net-tools
+yum -y install openldap compat-openldap openldap-clients openldap-servers openldap-servers-sql openldap-devel
+systemctl start slapd.service
+systemctl enable slapd.service
+netstat -antup | grep -i 389
 
